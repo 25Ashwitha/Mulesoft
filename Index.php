9f9302dafbc811390d695df9939dfe24a77928b0
25Ashwitha <90514260+25Ashwitha@users.noreply.github.com>
2022-06-17 14:42:03 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,7 +2,7 @@
     $server = "localhost";                    //server name
     $user = "root";                           //user name
     $password = "";                           //password is empty for sql
-    $db = "db_movies";                              //type database name
+    $db = "dbmovies";                              //type database name
 
     $conn = mysqli_connect($server,$user,$password,$db);    //connecting to database
 
@@ -10,11 +10,11 @@
     {
         echo "connection failed";       //exits the program
     }
-    $sql = "INSERT INTO movie (name , actor , actress , director , year ) VALUES ('ricky' , 'rakshit' , 'charlie' , 'shruthi' , 2003),('rocky' , 'yash' , 'nidhi' , 'neel' , 2022),('ASN' , 'ravi' , 'shreenidhi' , 'prashanth' , 2010),('vikram' , 'kamal' , 'pooja' , 'nelson' , 2022),('alone' , 'emran' , 'bipasha' , 'virappa' , 2001);       //query
+    $sql = "INSERT INTO movie (name , actor , actress , director , year ) VALUES ('ULIDAVARU KANDANTHE' , 'rakshit' , 'Rashmika' , 'Rakshith shetty' , 2022),('rocky' , 'yash' , 'nidhi' , 'neel' , 2022),('HELLO JUNIOR' , 'ravikumar' , 'sonakshi' , 'prashanth' , 2016),('real hero' , 'kamal' , 'pooja' , 'nelson' , 2002),('gold' , 'emran' , 'mouni' , 'virappa' , 2001);       //query
 
              $insert = mysqli_query($conn,$sql);                     //executing query
              echo '<script type="text/javascript">';
                 echo 'alert("movies added successfully");';
-                echo 'window.location.href="display.php";';
+                echo 'window.location.href="Display.php";';
                 echo "</script>";
 ?>
